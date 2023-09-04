54c1f04118f64aedbe8104601364bfd56d0b88b8
Dinesh kumar <dineshsoni8569@gmail.com>
2023-09-04 19:35:30 +0530

    commit 04

--- dataScraping/amazon.js
+++ dataScraping/amazon.js
@@ -1,8 +1,7 @@
 const getAmazonProduct = () => {
-  const img = getImg();
 };
+const img = getImg();
 getAmazonProduct();
-
 const getImg = () => {
   const imgs = document.querySelectorAll(".s-image");
   imgs.forEach((img) => console.log(img.currentSrc));
@@ -37,6 +36,28 @@ document
   .innerText.split(" ", 1);
 //const cs= document.querySelectorAll('.s-card-container.s-overflow-hidden.aok-relative.puis-expand-height.puis-include-content-margin.puis.puis-v2jxz9i2qjba5p2azxbyo8sltz5.s-latency-cf-section.s-card-border')
 // class=".s-card-container.s-overflow-hidden.aok-relative.puis-expand-height.puis-include-content-margin.puis.puis-v2jxz9i2qjba5p2azxbyo8sltz5.s-latency-cf-section.s-card-border"
-const ss = document.querySelectorAll(".a-section.a-spacing-base");
 const price = ss[8].childNodes[2].childNodes[2].childNodes[1].innerText;
 price.split(" ",1);
+
+if(arr[4].hasChildNodes()){
+  
+  const imgA = arr[4].children[1];
+  imgA.children[0].children[0].children[0].children[0].currentSrc;
+}
+
+// let imgA = arr[4].children[1];
+const ss = document.querySelectorAll(".a-section.a-spacing-base");
+let arr = [...ss];
+for (let i = 0; i < arr.length; i++) {
+  let imgA = arr[i].children[1];
+  imgA.children[0].children[0].children[0].children[0].currentSrc
+}
+// arr.forEach(it=> console.log(it.children[1]))
+
+arr.forEach(it=> {
+  let data = it.children[1];
+ let imgAr = data.children[0].children[0].children[0].children[0].currentSrc
+console.log(imgAr)
+})
+  
+  
